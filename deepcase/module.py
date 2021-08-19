b841b92fdd051f5935ea2499d4e9dc5757371d90
Thijs van Ede <t.s.vanede@utwente.nl>
2021-08-19 15:31:36 +0200

    Added DeepCASE object with fit, predict and fit_predict API.

--- deepcase/module.py
+++ deepcase/module.py
@@ -15,28 +15,36 @@ logger = logging.getLogger(__name__)
 
 class DeepCASE(object):
 
-    def __init__(self, n_features, context=10, complexity=128, device=None,
-                 eps=0.1, min_samples=5, threshold=0.2):
-        """Analyse security events with respect to contextual machine behaviour
+    def __init__(self,
+            n_features,
+            max_length  = 10,
+            hidden_size = 128,
+            eps         = 0.1,
+            min_samples = 5,
+            threshold   = 0.2,
+        ):
+        """Analyse security events with respect to contextual machine behaviour.
+
+            Note
+            ----
+            When an Interpreter is trained, it heavily depends on the
+            ContextBuilder used during training. Therefore, we **strongly**
+            suggest **not** to manually change the context_builder attribute,
+            without retraining the interpreter of the DeepCASE object.
 
             Parameters
             ----------
             n_features : int
-                Number of different possible security events
+                Number of different possible security events.
 
-            context : int, default=10
-                Maximum size of context window as number of events
+            max_length : int, default=10
+                Maximum length of context window as number of events.
 
-            complexity : int, default=128
-                Complexity of the network, describes the number of hidden
-                dimensions used by the sequence prediction algorithm.
-
-            device : 'cpu'|'cuda<i?>'|None, default=None
-                Hardware device to use for computation.
-                If None, fastest hardware is automatically inferred
-                If 'cuda' a specific graphics card can be selected using
-                cuda<i>, where <i> is the identifier of the graphics card, e.g.
-                'cuda0' for card 0.
+            hidden_size : int, default=128
+                Size of hidden layer in sequence to sequence prediction.
+                This parameter determines the complexity of the model and its
+                prediction power. However, high values will result in slower
+                training and prediction times.
 
             eps : float, default=0.1
                 Epsilon used for determining maximum distance between clusters.
@@ -48,30 +56,16 @@ class DeepCASE(object):
                 Minimum required confidence in fingerprint before using it in
                 training clusters.
             """
-        logger.info("DeepCASE.__init__(n_features={}, context={}, complexity={}"
-                     ", device={}, eps={}, min_samples={}, threshold={})"
-                     .format(n_features, context, complexity, device, eps,
-                     min_samples, threshold))
 
-        # Set device to use for prediction
-        if device is None:
-            # Automatically infer device
-            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        else:
-            self.device = device
-
-        # Initialise Context Builder
+        # Initialise Context Builder from parameters
         self.context_builder = ContextBuilder(
-            input_size    = n_features, # Possible different security events
-            output_size   = n_features, # Possible different security events
-            max_length    = context,    # Define maximum context window
-            hidden_size   = complexity, # Complexity of network (simple)
-            num_layers    = 1,          # Complexity of network (simple)
-            bidirectional = False,      # Complexity of network (simple)
-            LSTM          = False,      # Complexity of network (GRU/simple)
-        ).to(self.device)
+            input_size    = n_features,
+            output_size   = n_features,
+            max_length    = max_length,
+            hidden_size   = hidden_size,
+        )
 
-        # Initialise Interpreter
+        # Initialise Interpreter from parameters
         self.interpreter = Interpreter(
             context_builder = self.context_builder,
             features        = n_features,
@@ -80,79 +74,338 @@ class DeepCASE(object):
             threshold       = threshold,
         )
 
-        logger.info("DeepCASE successfully created")
-
-    ########################################################################
-    #                             Set methods                              #
-    ########################################################################
-
-    @property
-    def context_builder(self):
-        return self._context_builder
-
-    @context_builder.setter
-    def context_builder(self, context_builder):
-        # Set context_builder
-        self._context_builder = context_builder
-        # Set context_builder of interpreter
-        if hasattr(self, 'interpreter') and\
-           hasattr(self.interpreter, 'context_builder'):
-            self.interpreter.context_builder = context_builder
-
     ########################################################################
     #                              Fit method                              #
     ########################################################################
 
-    # TODO
+    def fit(self,
+            # Input data
+            X,
+            y,
+            scores,
+
+            # ContextBuilder-specific parameters
+            epochs        = 10,
+            batch_size    = 128,
+            learning_rate = 0.01,
+            optimizer     = optim.SGD,
+            teach_ratio   = 0.5,
+
+            # Interpreter-specific parameters
+            iterations       = 100,
+            query_batch_size = 1024,
+            strategy         = "max",
+            NO_SCORE         = -1,
+
+            # Verbosity level
+            verbose = True,
+        ):
+        """Fit DeepCASE with given data.
+            This method is provided as a wrapper and is equivalent to calling:
+             - context_builder.fit() and
+             - interpreter.fit()
+            in the given order.
+
+            Parameters
+            ----------
+            X : array-like of type=int and shape=(n_samples, context_size)
+                Input context to train with.
+
+            y : array-like of type=int and shape=(n_samples, n_future_events)
+                Sequences of target events.
+
+            scores : array-like of float, shape=(n_samples,)
+                Scores for each sample in cluster.
+
+            epochs : int, default=10
+                Number of epochs to train with.
+
+            batch_size : int, default=128
+                Batch size to use for training.
+
+            learning_rate : float, default=0.01
+                Learning rate to use for training.
+
+            optimizer : optim.Optimizer, default=torch.optim.SGD
+                Optimizer to use for training.
+
+            teach_ratio : float, default=0.5
+                Ratio of sequences to train including labels.
+
+            iterations : int, default=100
+                Number of iterations for query.
+
+            query_batch_size : int, default=1024
+                Size of batch for query.
+
+            strategy : string (max|min|avg), default=max
+                Strategy to use for computing scores per cluster based on scores
+                of individual events. Currently available options are:
+                - max: Use maximum score of any individual event in a cluster.
+                - min: Use minimum score of any individual event in a cluster.
+                - avg: Use average score of any individual event in a cluster.
+
+            NO_SCORE : float, default=-1
+                Score to indicate that no score was given to a sample and that
+                the value should be ignored for computing the cluster score.
+                The NO_SCORE value will also be given to samples that do not
+                belong to a cluster.
+
+            verbose : boolean, default=True
+                If True, prints progress.
+
+            Returns
+            -------
+            self : self
+                Returns self.
+            """
+
+        # Fit the ContextBuilder
+        self.context_builder.fit(
+            X             = X,
+            y             = y,
+            epochs        = epochs,
+            batch_size    = batch_size,
+            learning_rate = learning_rate,
+            optimizer     = optimizer,
+            teach_ratio   = teach_ratio,
+            verbose       = verbose,
+        )
+
+        # Fit the Interpreter
+        self.interpreter.fit(
+            X          = X,
+            y          = y,
+            scores     = scores,
+            iterations = iterations,
+            batch_size = query_batch_size,
+            strategy   = strategy,
+            NO_SCORE   = NO_SCORE,
+            verbose    = verbose,
+        )
+
+        # Returns self
+        return self
+
 
     ########################################################################
     #                            Predict method                            #
     ########################################################################
 
-    # TODO
+    def predict(self, X, y, iterations=100, batch_size=1024, verbose=False):
+        """Predict maliciousness of context samples.
+
+            Parameters
+            ----------
+            X : torch.Tensor of shape=(n_samples, seq_length)
+                Input context for which to predict maliciousness.
+
+            y : torch.Tensor of shape=(n_samples, 1)
+                Events for which to predict maliciousness.
+
+            iterations : int, default=100
+                Iterations used for optimization.
+
+            batch_size : int, default=1024
+                Batch size used for optimization.
+
+            verbose : boolean, default=False
+                If True, print progress.
+
+            Returns
+            -------
+            result : np.array of shape=(n_samples,)
+                Predicted maliciousness score.
+                Positive scores are maliciousness scores.
+                A score of 0 means we found a match that was not malicious.
+                Special cases:
+
+                * -1: Not confident enough for prediction
+                * -2: Label not in training
+                * -3: Closest cluster > epsilon
+            """
+        # Return the prediction of the interpreter
+        return self.interpreter.predict(
+            X          = X,
+            y          = y,
+            iterations = iterations,
+            batch_size = batch_size,
+            verbose    = verbose,
+        )
 
     ########################################################################
     #                         Fit/predict methods                          #
     ########################################################################
 
-    # TODO
+    def fit_predict(self,
+            # Input data
+            X,
+            y,
+            scores,
+
+            # ContextBuilder-specific parameters
+            epochs        = 10,
+            batch_size    = 128,
+            learning_rate = 0.01,
+            optimizer     = optim.SGD,
+            teach_ratio   = 0.5,
+
+            # Interpreter-specific parameters
+            iterations       = 100,
+            query_batch_size = 1024,
+            strategy         = "max",
+            NO_SCORE         = -1,
+
+            # Verbosity level
+            verbose = True,
+        ):
+        """Fit DeepCASE with given data and predict that same data.
+            This method is provided as a wrapper and is equivalent to calling:
+             - self.fit() and
+             - self.predict()
+            in the given order.
+
+            Parameters
+            ----------
+            X : array-like of type=int and shape=(n_samples, context_size)
+                Input context to train with.
+
+            y : array-like of type=int and shape=(n_samples, n_future_events)
+                Sequences of target events.
+
+            scores : array-like of float, shape=(n_samples,)
+                Scores for each sample in cluster.
+
+            epochs : int, default=10
+                Number of epochs to train with.
+
+            batch_size : int, default=128
+                Batch size to use for training.
+
+            learning_rate : float, default=0.01
+                Learning rate to use for training.
+
+            optimizer : optim.Optimizer, default=torch.optim.SGD
+                Optimizer to use for training.
+
+            teach_ratio : float, default=0.5
+                Ratio of sequences to train including labels.
+
+            iterations : int, default=100
+                Number of iterations for query.
+
+            query_batch_size : int, default=1024
+                Size of batch for query.
+
+            strategy : string (max|min|avg), default=max
+                Strategy to use for computing scores per cluster based on scores
+                of individual events. Currently available options are:
+                - max: Use maximum score of any individual event in a cluster.
+                - min: Use minimum score of any individual event in a cluster.
+                - avg: Use average score of any individual event in a cluster.
+
+            NO_SCORE : float, default=-1
+                Score to indicate that no score was given to a sample and that
+                the value should be ignored for computing the cluster score.
+                The NO_SCORE value will also be given to samples that do not
+                belong to a cluster.
+
+            verbose : boolean, default=True
+                If True, prints progress.
+
+            Returns
+            -------
+            result : np.array of shape=(n_samples,)
+                Predicted maliciousness score.
+                Positive scores are maliciousness scores.
+                A score of 0 means we found a match that was not malicious.
+                Special cases:
+
+                * -1: Not confident enough for prediction
+                * -2: Label not in training
+                * -3: Closest cluster > epsilon
+            """
+        # Call fit and predict in sequence and return result
+        return self.fit(
+            X                = X,
+            y                = y,
+            scores           = scores,
+            epochs           = epochs,
+            batch_size       = batch_size,
+            learning_rate    = learning_rate,
+            optimizer        = optimizer,
+            teach_ratio      = teach_ratio,
+            iterations       = iterations,
+            query_batch_size = query_batch_size,
+            strategy         = strategy,
+            NO_SCORE         = NO_SCORE,
+            verbose          = verbose,
+        ).predict(
+            X          = X,
+            y          = y,
+            iterations = iterations,
+            batch_size = query_batch_size,
+            verbose    = verbose,
+        )
+
+    ########################################################################
+    #                            Cast to device                            #
+    ########################################################################
+
+    def to(self, device):
+        """Cast DeepCASE to a specific device.
+            This method acts as a wrapper for the underlying context_builder.
+
+            Parameters
+            ----------
+            device : string
+                String describing the device, e.g., "cpu", "cuda", or "cuda:0".
+
+            Returns
+            -------
+            self : self
+                Returns self
+            """
+        # Cast ContextBuilder to device
+        self.context_builder = self.context_builder.to(device)
+
+        # Return self
+        return self
 
     ########################################################################
     #                             I/O methods                              #
     ########################################################################
 
-    def save(self, context_builder=None, interpreter=None):
-        """Save the DeepCASE object to output files.
-
-            Parameters
-            ----------
-            context_builder : string, optional
-                If given, output ContextBuilder to given file.
-
-            interpreter : string, optional
-                If given, output Interpreter to given file.
-            """
-        logger.info("DeepCASE.save(context_builder={}, interpreter={})".format(
-            context_builder, interpreter))
-
-        # Save individual objects
-        if context_builder is not None:
-            self.context_builder.save(context_builder)
-        if interpreter is not None:
-            self.interpreter.save(interpreter)
-
-    @classmethod
-    def load(cls, context_builder=None, interpreter=None):
-        logger.info("DeepCASE.load(context_builder={}, interpreter={})".format(
-            context_builder, interpreter))
-
-        # Load ContextBuilder
-        if context_builder is not None:
-            context_builder = ContextBuilder.load(context_builder)
-            print(context_builder)
-        # Load Interpreter
-        if interpreter is not None:
-            interpreter = Interpreter.load(interpreter, context_builder)
-            print(interpreter)
-
-        exit()
+    # def save(self, context_builder=None, interpreter=None):
+    #     """Save the DeepCASE object to output files.
+    #
+    #         Parameters
+    #         ----------
+    #         context_builder : string, optional
+    #             If given, output ContextBuilder to given file.
+    #
+    #         interpreter : string, optional
+    #             If given, output Interpreter to given file.
+    #         """
+    #     logger.info("DeepCASE.save(context_builder={}, interpreter={})".format(
+    #         context_builder, interpreter))
+    #
+    #     # Save individual objects
+    #     if context_builder is not None:
+    #         self.context_builder.save(context_builder)
+    #     if interpreter is not None:
+    #         self.interpreter.save(interpreter)
+    #
+    # @classmethod
+    # def load(cls, context_builder=None, interpreter=None):
+    #     logger.info("DeepCASE.load(context_builder={}, interpreter={})".format(
+    #         context_builder, interpreter))
+    #
+    #     # Load ContextBuilder
+    #     if context_builder is not None:
+    #         context_builder = ContextBuilder.load(context_builder)
+    #         print(context_builder)
+    #     # Load Interpreter
+    #     if interpreter is not None:
+    #         interpreter = Interpreter.load(interpreter, context_builder)
+    #         print(interpreter)
---
+++ example/example_module.py
@@ -0,0 +1,111 @@
+# Other imports
+import numpy as np
+import torch
+
+# DeepCASE Imports
+from deepcase.preprocessing import Preprocessor
+from deepcase               import DeepCASE
+
+if __name__ == "__main__":
+    ########################################################################
+    #                             Loading data                             #
+    ########################################################################
+
+    # Create preprocessor
+    preprocessor = Preprocessor(
+        length  = 10,    # 10 events in context
+        timeout = 86400, # Ignore events older than 1 day (60*60*24 = 86400 seconds)
+    )
+
+    # Load data from file
+    context, events, labels, mapping = preprocessor.csv('data/example.csv')
+
+    # In case no labels are provided, set labels to -1
+    if labels is None:
+        labels = np.full(events.shape[0], -1, dtype=int)
+
+    # Cast to cuda if available
+    if torch.cuda.is_available():
+        events  = events .to('cuda')
+        context = context.to('cuda')
+
+    ########################################################################
+    #                            Splitting data                            #
+    ########################################################################
+
+    # Split into train and test sets (20:80) by time - assuming events are ordered chronologically
+    events_train  = events [:events.shape[0]//5 ]
+    events_test   = events [ events.shape[0]//5:]
+
+    context_train = context[:events.shape[0]//5 ]
+    context_test  = context[ events.shape[0]//5:]
+
+    labels_train  = labels [:events.shape[0]//5 ]
+    labels_test   = labels [ events.shape[0]//5:]
+
+    ########################################################################
+    #                            Using DeepCASE                            #
+    ########################################################################
+
+    deepcase = DeepCASE(
+        # ContextBuilder parameters
+        n_features  = 300, # Number of input features to expect
+        max_length  = 10,  # Length of the context, should be same as context in Preprocessor
+        hidden_size = 128, # Number of nodes in hidden layer, in paper we set this to 128
+
+        # Interpreter parameters
+        eps         = 0.1, # Epsilon value to use for DBSCAN clustering, in paper this was 0.1
+        min_samples = 5,   # Minimum number of samples to use for DBSCAN clustering, in paper this was 5
+        threshold   = 0.2, # Confidence threshold used for determining if attention from the ContextBuilder can be used, in paper this was 0.2
+    )
+
+    # Cast to cuda if available
+    if torch.cuda.is_available():
+        deepcase = deepcase.to('cuda')
+
+    ########################################################################
+    #                             Fit DeepCASE                             #
+    ########################################################################
+
+    # Train the ContextBuilder
+    # Conveniently, the fit and fit_predict methods have the same API, so if you
+    # do not require the predicted values on the train dataset, simply
+    # substitute fit_predict with fit and it will run slightly quicker because
+    # DeepCASE skip the prediction over the training dataset and simply return
+    # the deepcase object itself. Other than that, both calls are exactly the
+    # same.
+    prediction_train = deepcase.fit_predict(
+        # Input data
+        X      = context_train,               # Context to train with
+        y      = events_train.reshape(-1, 1), # Events to train with, note that these should be of shape=(n_events, 1)
+        scores = labels_train,                # Labels used to compute score (either as loaded by Preprocessor, or put your own labels here)
+
+        # ContextBuilder-specific parameters
+        epochs        = 10,                   # Number of epochs to train with
+        batch_size    = 128,                  # Number of samples in each training batch, in paper this was 128
+        learning_rate = 0.01,                 # Learning rate to train with, in paper this was 0.01
+
+        # Interpreter-specific parameters
+        iterations       = 100,               # Number of iterations to use for attention query, in paper this was 100
+        query_batch_size = 1024,              # Batch size to use for attention query, used to limit CUDA memory usage
+        strategy         = "max",             # Strategy to use for scoring (one of "max", "min", "avg")
+        NO_SCORE         = -1,                # Any sequence with this score will be ignored in the strategy.
+                                              # If assigned a cluster, the sequence will inherit the cluster score.
+                                              # If the sequence is not present in a cluster, it will receive a score of NO_SCORE.
+
+        # Verbosity level
+        verbose = True,                       # If True, prints progress
+    )
+
+    ########################################################################
+    #                        Predict with DeepCASE                         #
+    ########################################################################
+
+    # Compute predicted scores
+    prediction_test = deepcase.predict(
+        X          = context_test,               # Context to predict
+        y          = events_test.reshape(-1, 1), # Events to predict, note that these should be of shape=(n_events, 1)
+        iterations = 100,                        # Number of iterations to use for attention query, in paper this was 100
+        batch_size = 1024,                       # Batch size to use for attention query, used to limit CUDA memory usage
+        verbose    = True,                       # If True, prints progress
+    )
